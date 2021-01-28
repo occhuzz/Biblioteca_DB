bc43ac1ff5315de394dcb18681489c7c1a48340f
Occhuzz <occhuzz@gmail.com>
2021-01-28 00:01:42 -0300

    Modificación de la estructura de Login y creación del usuario Administrador en el arranque

--- Biblioteca.c
+++ Biblioteca.c
@@ -1,22 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<time.h>
 
-typedef struct
+typedef struct login
 {
     char usuario[20];
     char password[20];
-
-} login;
+    struct login*sig;
+}  nodo;
 
 void cargarUsuarios()
 {
     FILE* archivoUsuarios;
+    char usuario[20];
+    char password[20];
 
     if(archivoUsuarios = fopen("log_usuarios.txt","r") == NULL)
     {
         printf("No existe el archivo de usuarios\n...Creando archivo de usuarios...\n");
         archivoUsuarios = fopen("log_usuarios.txt","a+");
+        printf("Ingrese un nombre de usuario para el Administrador: ");
+        gets(usuario);
+        printf("Ingrese el password para el Administrador: ");
+        gets(password);
+
+        printf("%s\t%s",usuario,password);
     }
     else
     {
@@ -25,11 +34,12 @@ void cargarUsuarios()
     }
     printf("Archivo de usuarios abierto");
 
-
+    fclose(archivoUsuarios);
 }
 
 int main()
 {
+    nodo*raiz=NULL;
     cargarUsuarios();
 
     return 0;
